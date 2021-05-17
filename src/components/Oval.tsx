b9eda4d0e3325738304553ab8efcdbc7ea7c6bce
Anthony Yarbrough <asyarb@hawaii.edu>
2021-05-17 13:56:35 -1000

    feat: setup ovals

---
+++ src/components/Oval.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+export const Oval = (props: React.ComponentProps<'svg'>) => {
+  return (
+    <svg viewBox="0 0 1440 3047" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <circle
+        cx={4893}
+        cy={4588}
+        fill="currentColor"
+        fillRule="evenodd"
+        r={4500}
+        transform="matrix(-1 0 0 1 5515 -88)"
+      />
+    </svg>
+  )
+}
