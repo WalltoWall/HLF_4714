4d7cd9c1def62624426ffa76367a69a47ee27998
Anthony Yarbrough <asyarb@hawaii.edu>
2021-05-18 17:15:36 -1000

    feat: favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,6 +19,11 @@ export const Layout = ({ children }: LayoutProps) => {
         <title>{settings.siteName}</title>
         <meta name="description" content={settings.siteDescription} />
         <link rel="stylesheet" href="https://use.typekit.net/erz3xap.css" />
+
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
       </Helmet>
 
       <SkipNavContent as="main">{children}</SkipNavContent>
