ac3850c531a55a972f6d63d839cbc0334dd34772
Anthony Yarbrough <asyarb@hawaii.edu>
2024-02-19 14:26:36 -1000

    feat: finish cleanup

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,7 +2,7 @@
 	import Bounded from "./Bounded.svelte"
 </script>
 
-<Bounded as="header" class="relative py-4 bg-white md:py-5 lg:py-6">
+<Bounded tag="header" class="relative py-4 bg-white md:py-5 lg:py-6">
 	<div class="flex justify-center">
 		<a href="/" class="outline-white">
 			<span class="sr-only">Navigate to home</span>
